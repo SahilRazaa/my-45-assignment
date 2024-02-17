691897b802fa261cdade9749bc04a7b310c53c22
SahilRazaa <159664371+SahilRazaa@users.noreply.github.com>
2024-02-17 21:14:59 +0500

    assignment 3

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
-var personName = "ERIC";
-console.log("hellow ".concat(personName, " would you like to learn some python today?"));
+var personName = "Sahil raza";
+console.log("lowecase:", personName.toLowerCase());
+// uppercase
+console.log("uppercase:", personName.toLocaleUpperCase());
+// titlecase
+console.log("titlecase:", personName.replace(/\bw/g, function (c) { return c.toUpperCase(); }));
--- app.ts
+++ app.ts
@@ -1,2 +1,9 @@
-let personName: string="ERIC";
-console.log(`hellow ${personName} would you like to learn some python today?`);
+let personName : string ="Sahil raza"
+console.log("lowecase:", personName.toLowerCase());
+
+// uppercase
+console.log("uppercase:",personName.toLocaleUpperCase());
+
+// titlecase
+
+console.log("titlecase:",personName.replace(/\bw/g,c =>c.toUpperCase())); 
